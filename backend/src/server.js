153739d7da2007e86ad1fe8346f222ad10d57fdf
Tebatso <tebatsomangena8@gmail.com>
2026-09-07 08:18:30 +0200

    Initial setup

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+import { ENV } from "./config/env.js";
+
+const app = express();
+const { PORT } = ENV;
+
+// middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Health check
+app.get("/", (req, res) => {
+  res.send("App running!!");
+});
+
+// Connect DB and listen to port
+app.listen(PORT, () => {
+  console.log(`Listening on PORT: ${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="bg-slate-100">
+      <p className="text-2xl font-bold">LetsStream</p>
+    </div>
+  )
+}
+
+export default App
